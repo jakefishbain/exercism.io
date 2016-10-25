db8ed2e2c9da2e0a41927b80c235dce1741613f0
Jake Fishbain <jakefishbain@gmail.com>
2016-10-25 14:44:02 -0500

    begin

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,9 @@
+class Hamming
+
+  def self.compute(s1, s2)
+    if s1 == s2
+      return 0
+    end
+  end
+
+end
--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -8,17 +8,14 @@ require_relative 'hamming'
 
 class HammingTest < Minitest::Test
   def test_identical_strands
-    # skip
     assert_equal 0, Hamming.compute('A', 'A')
   end
 
   def test_long_identical_strands
-    skip
     assert_equal 0, Hamming.compute('GGACTGA', 'GGACTGA')
   end
 
   def test_complete_distance_in_single_nucleotide_strands
-    skip
     assert_equal 1, Hamming.compute('A', 'G')
   end
 
