7c82bf34437269952540535fb6d63a91cc37ca8b
Jake Fishbain <jakefishbain@gmail.com>
2016-10-30 17:30:07 -0500

    write hamming method, pass all tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,8 +3,15 @@ class Hamming
   def self.compute(s1, s2)
     if s1 == s2
       return 0
-    elsif s1 != s2
-      return 1
+    else
+      pairs = s1.chars.zip(s2.chars)
+      sum = 0
+      pairs.each do |pair|
+        if pair[0] != pair[1]
+          sum += 1
+        end
+      end
+      return sum
     end
   end
 
