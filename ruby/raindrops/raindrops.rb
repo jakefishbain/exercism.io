43ef53fce9c2c18c2acbc1d9935067b649703d27
Jake Fishbain <jakefishbain@gmail.com>
2016-12-13 09:09:15 -0600

    convert n to string for output

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,11 +1,15 @@
 class Raindrops
   def self.convert(n)
-    if n/7 == (n/7)*n
+    if n == 1
+      return n.to_s
+    elsif n/7 == (n/7)*n
       return 'Plong'
     elsif n/5 == (n/5)*n
       return 'Plang'
     elsif n/3 == (n/3)*n
       return 'Pling'
+    else 
+      return n
     end
   end
 end
