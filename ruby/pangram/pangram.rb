679265fb7d2790cdb2f21b9fbb2e2018f98783a3
Jake Fishbain <jakefishbain@gmail.com>
2016-12-13 17:05:07 -0600

    iterate through alphabet and check phrase

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,3 +1,10 @@
 class Pangram
-  
+
+  def self.pangram?(phrase)
+    alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+    phrase_array = phrase.split('')
+    alphabet.each { |letter| return false unless phrase_array.include?(letter) }
+  end
 end
+
+# puts Pangram.pangram?('I love lamp')
