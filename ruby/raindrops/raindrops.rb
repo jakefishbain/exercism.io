d10dd196e3a81f87930871f941a6e96cd53195e8
Jake Fishbain <jakefishbain@gmail.com>
2016-12-13 09:14:37 -0600

    remove math from first side of the equation

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,11 +2,11 @@ class Raindrops
   def self.convert(n)
     if n == 1
       return n.to_s
-    elsif n/7 == (n/7)*n
+    elsif n == (n/7.0)*n
       return 'Plong'
-    elsif n/5 == (n/5)*n
+    elsif n == (n/5.0)*n
       return 'Plang'
-    elsif n/3 == (n/3)*n
+    elsif n == (n/3.0)*n
       return 'Pling'
     else 
       return n
