6c9f188160c2118027c3e45bbcd8703b9a0f3cd7
Jake Fishbain <jakefishbain@gmail.com>
2017-01-26 10:52:24 -0600

    fix total method

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -4,4 +4,14 @@ class Grains
     return grains if n == 1
     self.square(n-1, grains*2)
   end
+
+  def self.total
+    total = 0
+    n = 1
+    until n > 64
+      total += self.square(n)
+      n += 1
+    end
+    return total
+  end
 end
