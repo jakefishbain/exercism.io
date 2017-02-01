e1e6bb1175a7d29427e6e29785747e40a1091e7f
Jake Fishbain <jakefishbain@gmail.com>
2017-02-01 09:44:07 -0500

    pass expanded lists

--- ruby/word-count/word_count_test.rb
+++ ruby/word-count/word_count_test.rb
@@ -31,7 +31,6 @@ class PhraseTest < Minitest::Test
   end
 
   def test_handles_expanded_lists
-    skip
     phrase = Phrase.new("one,\ntwo,\nthree")
     counts = {"one"=>1, "two"=>1, "three"=>1}
     assert_equal counts, phrase.word_count
