00e8c2abc40f04083c35bb0ff0cbb7c2432faa8e
Jake Fishbain <jakefishbain@gmail.com>
2017-03-12 14:02:32 -0500

    pass question

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,6 +1,8 @@
 class Bob
   def self.hey(remark)
-    if remark == remark.upcase
+    if remark.split('').last == '?'
+      return 'Sure.'
+    elsif remark == remark.upcase
       return 'Whoa, chill out!'
     else
       return 'Whatever.'
