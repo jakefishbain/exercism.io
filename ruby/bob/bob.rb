27bd93b053756de2f9d8cb18196a05af978e49a0
Jake Fishbain <jakefishbain@gmail.com>
2017-03-12 14:18:40 -0500

    pull features out into methods and pass all tests

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,11 +1,25 @@
 class Bob
   def self.hey(remark)
-    if remark.split('').last == '?'
-      return 'Sure.'
-    elsif remark == remark.upcase
+  if silence?(remark)
+      return 'Fine. Be that way!'
+    elsif shouting?(remark)
       return 'Whoa, chill out!'
+    elsif asking?(remark)
+      return 'Sure.'
     else
       return 'Whatever.'
     end
+  end
+
+  def self.shouting?(remark)
+    remark == remark.upcase && remark != remark.downcase
+  end
+
+  def self.asking?(remark)
+    remark.end_with?('?')
+  end
+
+  def self.silence?(remark)
+    remark.strip.empty?
   end
 end
