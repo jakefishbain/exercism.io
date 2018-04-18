d8c0602bf0e8c0b4709f717d340aa24bb273da93
jakefishbain <jakefishbain@gmail.com>
2018-04-17 20:43:53 -0500

    submit phone number, get prime factors

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,18 +1,15 @@
 class PhoneNumber
   def self.clean(number)
-    arr = []
-    num_arr = number.split('')
-    num_arr.each do |char| 
-      arr << char if (char =~ /\d/) === 0
-    end
-    arr.shift if arr[0] === '1'
-    arr.join
+    clean_num = number.gsub(/^1|\D+1|\D/, '')
+    valid?(clean_num) ? clean_num : nil
+  end
+
+  def self.valid?(num)
+    (2..9).to_a.include?(num[3].to_i) &&
+    num.length === 10
   end
 end
 
 module BookKeeping
   VERSION = 2
 end
-
-num = "+1 (223) 456-7890"
-p PhoneNumber.clean(num)
