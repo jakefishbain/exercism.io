baa4e723c1bdcf36b0cba2bd153014fcba4cf9e9
jakefishbain@gmail.com <jakefishbain@gmail.com>
2018-05-06 21:40:49 -0500

    submit bob, fetch gigasecond

--- ecmascript/bob/bob.js
+++ ecmascript/bob/bob.js
@@ -6,10 +6,31 @@
 
 class Bob {
   hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (message.trim().length === 0) {
+      return 'Fine. Be that way!'
+    }else if(message.endsWith('?') && isYelling(message) && notAllNumbers(message)){
+      return 'Calm down, I know what I\'m doing!'
+    }else if(message.endsWith('?')) {
+      return 'Sure.'
+    }else if(isYelling(message) && notAllNumbers(message)) {
+      return 'Whoa, chill out!'
+    }else {
+      return 'Whatever.'
+    }
   }
+}
+
+const isYelling = (msg) => {
+  return !msg.split('').map((letter) => {
+    return letter === letter.toUpperCase()
+  }).some(x => x === false)
+}
+
+const notAllNumbers = (msg) => {
+  const stripped = msg.split('').filter( x => x != ',')
+  return stripped.map((letter) => {
+    return (isNaN(letter) && letter != '?')
+  }).some(x => x === true)
 }
 
 export default Bob;
